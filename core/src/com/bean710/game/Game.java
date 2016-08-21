8545f1cb3ce6945d57b8568fdd9960ede991af5a
Ben Keener <keener4christ@gmail.com>
2016-08-21 15:08:15 -0400

    Successfully built Zombie version 1.0

--- core/src/com/bean710/game/Game.java
+++ core/src/com/bean710/game/Game.java
@@ -6,19 +6,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
 import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.UBJsonReader;
 
@@ -34,7 +30,7 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	public void create() {
 		// Set up camera
 		camera = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		camera.position.set(0f, 100f, 100f);
+		camera.position.set(0f, 100f, 150f);
 		camera.lookAt(0f, 100f, 0f);
 		camera.near = 0.1f;
 		camera.far = 300f;
@@ -60,10 +56,14 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	@Override
 	public void render() {
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		
 		camera.update();
+		controller.update(Gdx.graphics.getDeltaTime());
+		
+		
+		
 		modelBatch.begin(camera);
 		modelBatch.render(modelInstance);
 		modelBatch.end();
