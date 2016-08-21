373a7c2cd9c1e7b5fd829ee90a987fc3ac1b8d21
Ben Keener <keener4christ@gmail.com>
2016-08-21 13:49:32 -0400

    Working 3D box

--- core/src/com/bean710/game/Game.java
+++ core/src/com/bean710/game/Game.java
@@ -2,6 +2,9 @@ package com.bean710.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -13,8 +16,9 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
 
-public class Game extends ApplicationAdapter {
+public class Game extends ApplicationAdapter implements InputProcessor {
 	private PerspectiveCamera camera;
 	private ModelBatch modelBatch;
 	private ModelBuilder modelBuilder;
@@ -39,16 +43,74 @@ public class Game extends ApplicationAdapter {
 		modelInstance = new ModelInstance(box, 0, 0, 0); // Create instance of 'box'
 		
 		environment = new Environment(); // Make the game's environment
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, ));
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f, 0.8f, 0.8f, 1f));
+		
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		
+		camera.update();
+		modelBatch.begin(camera);
+		modelBatch.render(modelInstance);
+		modelBatch.end();
+		
 	}
 
 	@Override
 	public void dispose() {
+		box.dispose();
+		modelBatch.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		//TODO MAKE THESE TEMPORARY VARIABLES
+		if (keycode == Input.Keys.LEFT) {
+			camera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), 10f);
+		}
+		if (keycode == Keys.RIGHT) {
+			camera.rotateAround(new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), -10f);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
